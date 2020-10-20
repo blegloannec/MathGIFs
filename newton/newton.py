c01ddccabe696e6e03c06cce22a5be4f5b6777c5
blegloannec <blg@gmx.com>
2020-10-20 22:45:34 +0200

    newton

--- newton/newton.py
+++ newton/newton.py
@@ -6,21 +6,23 @@ import os, random
 random.seed()
 
 EPS = 1e-6
-randcol = lambda: tuple(random.randint(0,255) for _ in range(3))
+randcol = lambda: tuple(random.randint(100,255) for _ in range(3))
+random_colors = lambda n: [randcol() for _ in range(n)]
 
 
 class NewtonImg:
-    def __init__(self, Poly, radius=30., size=801, iterations=20):
+    def __init__(self, Poly, x1=-10.,y1=-10., x2=10.,y2=10., width=801,height=801, iterations=20):
         self.P = np.array(Poly).astype('f')
         self.DP = np.polyder(self.P)
         self.Roots = np.roots(self.P)
-        self.size = size
+        self.width  = width
+        self.height = height
         self.itmax = iterations
         self.Z = []
-        for iy in range(size):
-            y = 2*radius*iy/size - radius
-            for ix in range(size):
-                x = 2.*radius*ix/size - radius
+        for iy in range(self.height-1,-1,-1):
+            y = ((self.height-iy)*y1 + iy*y2) / self.height
+            for ix in range(self.width):
+                x =((self.width-ix)*x1 + ix*x2) / self.width
                 self.Z.append(complex(x,y))
         self.Z = np.array(self.Z)
         self.Iter = np.array([0]*self.Z.size)
@@ -32,20 +34,20 @@ class NewtonImg:
 
     def gray(self):
         Data = 255 * (self.itmax-self.Iter) // self.itmax
-        Img = Image.new('L', (self.size, self.size))
+        Img = Image.new('L', (self.width, self.height))
         Img.putdata(Data)
         return Img
     
     def rgb(self, Colors=None):
         if Colors is None:
-            Colors = [randcol() for _ in range(self.Roots.size)]
+            Colors = random_colors(self.Roots.size)
         else:
             assert len(Colors)>=self.Roots.size
         Dist = np.array([np.abs(self.Z-r) for r in self.Roots])
         Data = np.argmin(Dist, axis=0)
         RGB = [np.array([Colors[c][i] for c in Data]) for i in range(3)]
         RGB = RGB * (self.itmax - self.Iter) // self.itmax
-        Img = Image.new('RGB', (self.size, self.size))
+        Img = Image.new('RGB', (self.width, self.height))
         Data = list(zip(*RGB))
         Img.putdata(Data)
         return Img
@@ -53,11 +55,12 @@ class NewtonImg:
 
 # Anim.
 def random_newton_anim(degree=3, frames=20, radius=20., size=201, iterations=30):
+    Colors = random_colors(degree)
     P = [4.*random.random()-2. for _ in range(degree)] + [1.]
     for t in range(frames):
         for i in range(degree):
             P[i] += 0.03
-        Img = NewtonImg(P, radius, size, iterations).rgb()
+        Img = NewtonImg(P, -radius,-radius, radius,radius, size,size, iterations).rgb(Colors)
         Img.save('/tmp/frame%04d.gif' % t)
     os.system('gifsicle -O3 -l -d10 /tmp/frame*.gif > anim.gif')
     os.system('rm -f /tmp/frame*.gif')
@@ -65,12 +68,14 @@ def random_newton_anim(degree=3, frames=20, radius=20., size=201, iterations=30)
 
 # MAIN
 if __name__=='__main__':
-    #P = (1, 0, 0, -1)
-    #P = (1,0,0,0,-1)
-    #P = (1,0,0,0,15,0,0,0,-16)
-    P = (1, 0 ,-300, -3000)
-    Colors = [(255,0,0), (0,255,0), (0,0,255)]
+    #P = (1, 0, 0, -1)  # X³-1
+    #P = (1, 0, 0, 0, -1)
+    #P = (1, 0, -2, 2)  # example with non converging areas
+    #P = (1, 0, 0, 0, 15, 0, 0, 0, -16)
+    #P = (1, 0 ,-300, -3000)
+    P = tuple([1]+[random.randint(-5,5) for _ in range(3)])
+    Colors = [(255,100,100), (100,255,100), (100,100,255)]
+    #Img = NewtonImg(P, x1=-15.,y1=-20., x2=25.,y2=20., iterations=25).rgb()
     Img = NewtonImg(P).rgb(Colors)
     Img.save('out.png')
-    #Img = np_newton(tuple([1]+[random.randint(-2,2) for i in range(3)]))
     #random_newton_anim(3)
