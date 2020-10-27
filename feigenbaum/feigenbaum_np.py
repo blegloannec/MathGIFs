d8e38b350ea25527a29fa99ceeeb871ec8ab8d5b
blegloannec <blg@gmx.com>
2020-10-27 18:18:33 +0100

    fei. (np)

---
+++ feigenbaum/feigenbaum_np.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def feigenbaum(Imu=4000, Iter=400):
+    Mu = np.linspace(2., 4., Imu)
+    X = np.array([0.5]*Imu)
+    for i in range(Iter):
+        X = Mu * X * (1.-X)
+        if 2*i>Iter:
+            plt.plot(Mu, X, ',')  # ',k' for black
+    plt.show()
+
+def feigenbaum2(Imu=4000, Iter=400):
+    Mu = np.linspace(1., 20., Imu)
+    X = np.array([0.5]*Imu)
+    for i in range(Iter):
+        X = Mu * X * (1.-np.tanh(X))
+        if 2*i>Iter:
+            plt.plot(Mu, X, ',')
+    plt.show()
+
+feigenbaum()
